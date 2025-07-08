fa11bcb3dbbb06156ea3da666afb371e5121b3b4
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 20:39:21 +0500

    Add src\pages\Home.js

---
+++ src\pages\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to Student Management System</h1>
+      <p>This system allows you to manage student data.</p>
+    </div>
+  );
+};
+
+export default Home;
