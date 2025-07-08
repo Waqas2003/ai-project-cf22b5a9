67eafa60f8e4c812e43d851b7df62e9f10fbc858
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 20:39:23 +0500

    Add src\pages\Students.js

---
+++ src\pages\Students.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Students = ({ students }) => {
+  return (
+    <div className="container">
+      <h1>Students</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(student => (
+            <tr key={student.id}>
+              <td>{student.id}</td>
+              <td>{student.name}</td>
+              <td>{student.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Students;
